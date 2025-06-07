070ae10f8cc549f8872351b45677642fdc6a1fe6
Ebube Ezedimbu <eezedimbu@gmail.com>
2025-06-07 21:17:11 +0100

    Complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,19 +4,19 @@ import { BsTwitterX } from "react-icons/bs";
 import { FaInstagram } from "react-icons/fa6";
 const Footer = () => {
   return (
-    <div className="flex justify-between items-center px-5 md:px-8 py-7 md:py-13 bg-[#F3F3F3]">
-        <img src="public/Planty's logo.svg" alt="planty-logo" />
+    <div className="flex flex-col md:flex-row justify-between items-center px-5 md:px-8 py-7 md:py-13 bg-[#F3F3F3] gap-10 md:gap-0">
+        <img src="public/Planty's logo.svg" alt="planty-logo" className="transform -translate-y-1.5"/>
 
-        <section className="flex items-center gap-10 font-lora">
+        <section className="flex flex-col md:flex-row gap-9 items-center md:gap-7 lg:gap-10 font-lora">
             <p>Help</p>
             <p>Contact Us</p>
             <p>Privacy &amp; Terms</p>
         </section>
 
-        <section className="flex items-center gap-10">
-            <FaFacebook className="w-6 h-6"/>
-            <BsTwitterX className="w-6 h-6"/>
-            <FaInstagram className="w-6 h-6" />
+        <section className="flex items-center gap-15 md:gap-7 lg:gap-10">
+            <FaFacebook className="w-8 h-8 md:h-5.5 md:w-5.5 lg:w-6 lg:h-6"/>
+            <BsTwitterX className="w-8 h-8 md:h-5.5 md:w-5.5 lg:w-6 lg:h-6"/>
+            <FaInstagram className="w-8 h-8 md:h-5.5 md:w-5.5 lg:w-6 lg:h-6" />
         </section>
       
     </div>
--- src/components/Lower.jsx
+++ src/components/Lower.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 const Lower = () => {
   return (
-    <div className="px-5 md:px-8 mb-5">
-        <section className="flex items=-center gap-8 md:h-190">
-            <img src="/src/img/bottom-1.png" alt="plant-1" className=""/>
-            <img src="/src/img/bottom-2.png" alt="" />
+    <div className="px-5 md:px-8 mb-6 md:mb-10 lg:mb-20">
+        <section className="flex items=-center gap-4 lg:gap-5 md:h-140 lg:h-170">
+            <img src="/src/img/bottom-1.png" alt="plant-1" className="w-[38%]"/>
+            <img src="/src/img/bottom-2.png" alt="plant-2" className="w-[60%]"/>
         </section>
 
-        <img src="/src/img/bottom-3.png" alt="" className="my-5 md:my-10 "/>
+        <img src="/src/img/bottom-3.png" alt="plant-3" className="my-5 md:my-10 "/>
       
-      <section className="flex justify-between items-center gap-10 w-full">
-        <h1 className="font-lora font-medium text-4xl w-[50%] leading-12">Subscribe to our newsletter and stay updated with special offers, plant-parenting tips, and more.</h1>
+      <section className="flex flex-col md:flex-row justify-between items-center gap-5 md:gap-10 w-full">
+        <h1 className="font-lora font-medium text-xl md:text-2xl lg:text-4xl text-center md:text-left w-[80%] lg:w-[60%] xl:w-[50%] leading-10 md:leading-12">Stay updated with special offers, plant-parenting tips, and more.</h1>
 
-        <div className="w-[35%] after:content-[''] after:w-[95%] after:h-0.5 after:bg-black relative after:absolute after:-bottom-2 after:-left-1" >
-            <input type="email" name="email" id="email" placeholder='Your email' className="font-lora text-[16px] px-2 focus:outline-none focus:ring-0 inline mr-10"/>
-            <button className="text-white font-dmsans font-medium text-sm bg-black px-10 rounded-full h-9 inline">Subscribe</button>
+        <div className="flex items-center w-[35#%] border-b-2 pb-2 transform lg:translate-y-5 xl:translate-y-5" >
+            <input type="email" name="email" id="email" placeholder='Your email' className="font-lora text-[16px] px-2 focus:outline-none focus:ring-0 xl:mr-10"/>
+            <button className="text-white font-dmsans font-medium text-xs md:text-sm bg-black px-5 lg:px-7 xl:px-10 rounded-full h-8 md:h-9">Subscribe</button>
         </div>
       </section>
     </div>
